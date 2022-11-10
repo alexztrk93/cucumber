fe37a091b57e006e9638979df56910b640accbf2
alexztrk93 <alexztrk55@gmail.com>
2022-11-10 15:52:29 -0500

    driver-remote driver-driver pool singleton in driver util class

--- src/test/java/com/cydeo/utilities/Driver.java
+++ src/test/java/com/cydeo/utilities/Driver.java
@@ -49,7 +49,7 @@ public class Driver {
                 case "remote-chrome":
                     try {
                         // assign your grid server address
-                        String gridAddress = "174.129.57.206";
+                        String gridAddress = "54.166.224.149";
                         URL url = new URL("http://"+ gridAddress + ":4444/wd/hub");
                         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
                         desiredCapabilities.setBrowserName("chrome");
